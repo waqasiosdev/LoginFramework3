148bb8581062721f0e08fe2ec3f30a4e97c9c6e4
Waqas Sarwar <waqas.sarwar@coeus-solutions.de>
2022-12-08 18:41:43 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LoginFramework
+
+A description of this package.
---
+++ Sources/LoginFramework/LoginFramework.swift
@@ -0,0 +1,6 @@
+public struct LoginFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
