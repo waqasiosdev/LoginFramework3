72482cc755697a3571c45d9cedc9241676e82a56
Waqas Sarwar <waqas.sarwar@coeus-solutions.de>
2022-12-08 18:50:45 +0500

    initial commit

--- Package.swift
+++ Package.swift
@@ -18,11 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LoginFramework",
-            dependencies: []),
-        .testTarget(
-            name: "LoginFrameworkTests",
-            dependencies: ["LoginFramework"]),
+        .binaryTarget(
+                    name: "LoginFramework",
+                    path: "./Sources/LoginFramework.xcframework")
     ]
 )
